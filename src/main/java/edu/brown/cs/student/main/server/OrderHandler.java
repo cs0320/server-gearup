a94eef370226c9342ee8372f7ab00716875d4bd9
jmaffa <90526195+jmaffa@users.noreply.github.com>
2024-02-08 11:02:02 -0500

    minor refactor: removed space in Number of ingredients

--- src/main/java/edu/brown/cs/student/main/server/OrderHandler.java
+++ src/main/java/edu/brown/cs/student/main/server/OrderHandler.java
@@ -53,7 +53,7 @@ public class OrderHandler implements Route {
     // Iterate through the soups in the menu and return the first one
     for (Soup soup : this.menu) {
       responseMap.put(soup.getSoupName(), soup);
-      responseMap.put("Number of  ingredients", soup.getIngredients().size());
+      responseMap.put("Number of ingredients", soup.getIngredients().size());
       return new SoupSuccessResponse(responseMap).serialize();
     }
     return new SoupNoRecipesFailureResponse().serialize();
