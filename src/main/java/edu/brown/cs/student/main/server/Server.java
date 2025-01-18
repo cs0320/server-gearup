d8c169362faec636340d4ae51cde5a089763ad58
Andrew Chen <andrewzchen66@gmail.com>
2025-01-18 15:49:26 -0500

    updated api endpoints to show

--- src/main/java/edu/brown/cs/student/main/server/Server.java
+++ src/main/java/edu/brown/cs/student/main/server/Server.java
@@ -61,7 +61,7 @@ public class Server {
 
     // Setting up the handler for the GET /order and /activity endpoints
     Spark.get("order", new OrderHandler(menu));
-    Spark.get("weather", new ActivityHandler());
+    Spark.get("show", new TVShowHandler());
     Spark.init();
     Spark.awaitInitialization();
 
